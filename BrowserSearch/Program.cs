b6dcfddaa52f75474884a8e54dcf4035472dcfd2
Stef Kariotidis <stef.kariotidis@gmail.com>
2020-10-11 09:50:10 +0300

    Add project files.

---
+++ BrowserSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BrowserSearch
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            string url = "https://google.com?q=";
+            string content = Clipboard.GetText();
+
+            if (content != "")
+            {
+                System.Diagnostics.Process.Start(url + content);
+            }
+
+        }
+    }
+}
